ba57dcfefca691381f0aca315846e9adc6dd4876
heywon0909 <50330312+heywon0909@users.noreply.github.com>
2021-07-17 16:41:57 +0900

    Unreal Webcam Fun
    javascript19일 unreal webcam Fun 프로젝트

---
+++ unreal webcam fun/script.js
@@ -0,0 +1,104 @@
+const video = document.querySelector('.player');
+const canvas =document.querySelector('.photo');
+const ctx = canvas.getContext('2d');
+const strip = document.querySelector('.strip');
+const snap = document.querySelector('.snap');
+
+function getVideo(){
+    //navigator.getUserMedia()를 통해서 플러그인 없이 웹캠과 마이크입력을 건드릴 수 있다.
+    navigator.mediaDevices.getUserMedia({video: true,audio: false})
+    .then(localMediaStream =>{
+      video.srcObject = localMediaStream;
+      video.play();
+    })
+    .catch(err =>{
+        console.error(`oh ho!!!`,err);
+    });
+}
+
+function paintToCanvas(){
+    const width =video.videoWidth;
+    const height = video.videoHeight;
+    canvas.width = width;
+    canvas.height = height;
+
+    return setInterval(() => {
+        
+        ctx.drawImage(video,0,0,width,height);
+        // take the pixels out
+        let pixels = ctx.getImageData(0,0,width,height);
+        // mess with them
+        //pixels = redEffect(pixels);
+        //spixels = rgbSplit(pixels);
+        //ctx.globalAlpha = 0.8;
+        // put them back
+        pixels = greenScreen(pixels);
+        ctx.putImageData(pixels,0,0);
+        
+    }, 16);
+    
+}
+
+
+function takePhoto(){
+    //play the sound
+    snap.currentTime = 0;
+    snap.play();
+    // take the data out of the canvas
+    const data = canvas.toDataURL('image/jpeg');
+    const link = document.createElement('a');
+    link.href = data;
+    link.setAttribute('download','handsome');
+    link.textContent = 'Download Image';
+    link.innerHTML = `<img src="${data}" alt="Handsome Man"/>`;
+    strip.insertBefore(link,strip.firstChild);
+}
+
+function redEffect(pixels){
+    for(let i=0;i<pixels.data.length; i+=4){
+        pixels.data[i+0] = pixels.data[i+0]+ 100;
+        pixels.data[i+1] = pixels.data[i+1] - 50;
+        pixels.data[i+2] = pixels.data[i+2] * 0.5;
+    }
+    return pixels;
+}
+function rgbSplit(pixels){
+    for(let i=0; i<pixels.data.length; i+=4){
+        pixels.data[i -150] = pixels.data[i+0];
+        pixels.data[i  + 100] = pixels.data[i+1];
+        pixels.data[i-150] = pixels.data[i+2];   
+    }
+    return pixels;
+}
+function greenScreen(pixels) {
+    const levels = {};
+  
+    document.querySelectorAll('.rgb input').forEach((input) => {
+      levels[input.name] = input.value;
+    });
+  
+    for (i = 0; i < pixels.data.length; i = i + 4) {
+      red = pixels.data[i + 0];
+      green = pixels.data[i + 1];
+      blue = pixels.data[i + 2];
+      alpha = pixels.data[i + 3];
+  
+      if (red >= levels.rmin
+        && green >= levels.gmin
+        && blue >= levels.bmin
+        && red <= levels.rmax
+        && green <= levels.gmax
+        && blue <= levels.bmax) {
+        // take it out!
+        pixels.data[i + 3] = 0;
+      }
+    }
+  
+    return pixels;
+  }
+  
+getVideo();
+
+
+video.addEventListener('canplay',paintToCanvas);
+
